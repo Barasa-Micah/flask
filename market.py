6182a03d047f77f9a0f25546a37531f33dc041c1
username <barasamica@gmai.com>
2024-05-04 13:03:15 +0300

    Added some jinja template

--- market.py
+++ market.py
@@ -9,4 +9,9 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    return render_template('market.html', item_name='phone')
+    items= [
+        {'id': 1, 'name': 'phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    return render_template('market.html', items = items)
